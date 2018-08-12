33e3fe158236d53538a3f26b07703f468e3f4fc4
kevinl622@gmail.com <kevinl622@gmail.com>
2018-08-12 13:41:30 -0700

    Solution Code to Reverse Integer

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -3,5 +3,30 @@
  * @return {number}
  */
 var reverse = function(x) {
+    let  number = x;
+    let digitsArray = ("" + number).split("");
 
+    let reverseArray = digitsArray.reverse();
+
+    if(reverseArray[0] == 0 && reverseArray.length > 1) {
+        reverseArray.shift();
+    }
+
+    if(reverseArray[reverseArray.length - 1] == '-') {
+        reverseArray.pop();
+        reverseArray.unshift('-');
+    }
+
+    let output = reverseArray.join("");
+    output = parseInt(output);
+    if (output >= 2147483647 || output <= -2147483648){
+        return 0;
+    } else {
+        return output;
+    }
 };
+
+reverse(1230);
+reverse(-123);
+reverse(2147483647);
+reverse(-2147483649);
