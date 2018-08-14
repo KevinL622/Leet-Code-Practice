aeb18a82480327f2c1aec561449e35794b8cf34c
kevinl622@gmail.com <kevinl622@gmail.com>
2018-08-13 18:36:26 -0700

    Added Solution Code

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -2,6 +2,39 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
 
+//this solution checks if a number is a palindrome without using any strings
+var isPalindrome = function(x) {
+    //check for negative inputs
+    if (x < 0) {
+        return false;
+    } else if (x < 10) {
+        //numbers less than 10 and greater than 0 are palindromes
+        return true;
+    } else {
+        var array = [];
+        //push first digit of the number into array
+        array.push(x % 10);
+        var copy = x;
+        //isolate each digit of the number using division/modulo then push
+        while (copy >= 10) {
+            copy = Math.floor(copy/10);
+            var number = copy % 10;
+            array.push(number);
+        }
+        var arrayReverse = array.slice();
+        arrayReverse.reverse();
+        //check if the indices values of the reversed array and array are equal
+            for(var i=0; i<array.length; i++) {
+                if (array[i] != arrayReverse[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
 };
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(0));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(45));
