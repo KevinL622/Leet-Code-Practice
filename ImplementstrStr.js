af28044aa2d8baeefb16ebc10e37552a2c961757
kevinl622@gmail.com <kevinl622@gmail.com>
2018-08-15 16:46:13 -0700

    Added Solution Code with Comments

--- ImplementstrStr.js
+++ ImplementstrStr.js
@@ -3,6 +3,20 @@
  * @param {string} needle
  * @return {number}
  */
+ //Function returns indice of the first occurence of a substring in a string
 var strStr = function(haystack, needle) {
+   var string = haystack;
+   var substring = needle;
 
+   if(substring == '') {
+       return 0; //return 0 for empty substrings
+   } else if (string.includes(substring)) {
+       return string.indexOf(substring);
+   } else {
+       return -1;//return -1 if substring isn't in the string
+   }
 };
+
+console.log(strStr('mississippi', 'issip')); //returns 4
+console.log(strStr('string', '')); //returns 0
+console.log(strStr('aaaaa', 'bba')); //returns -1
