6977835c33ff7698cd38eb3a05609acb0dc775c0
kevinl622@gmail.com <kevinl622@gmail.com>
2018-09-13 12:10:34 -0700

    Updated Solution Code

--- TwoSum.js
+++ TwoSum.js
@@ -17,6 +17,23 @@ return [0, 1].
 
 /* Approaches
 brute force double for loop O(n^2) worst case
+// our two sum function which will return
+// all pairs in the array that sum up to S
+function twoSum(arr, S) {
+  var sums = [];
+  // check each element in array
+  for (var i = 0; i < arr.length; i++) {
+    // check each other element in the array
+    for (var j = i + 1; j < arr.length; j++) {
+      // determine if these two elements sum to S
+      if (arr[i] + arr[j] === S) {
+        sums.push([arr[i], arr[j]]);
+      }
+    }
+  }
+  // return all pairs of integers that sum to S
+  return sums;
+}
 
 use a hash table O(n)
 
@@ -28,7 +45,7 @@ use a hash table O(n)
 var twoSum = function(nums, target) {
     var pairIndex = [];//if you want the indices
     var pairNumber = [];//if you want the numbers themselves
-    var hash = {};
+    var hash = {};//intitialize a hash table
 
     for(var i=0; i < nums.length; i++){
         var difference = target - nums[i];
@@ -36,6 +53,7 @@ var twoSum = function(nums, target) {
             pairIndex.push(i, nums.indexOf(difference));
             pairNumber.push(nums[i], difference)
         }
+        //add to hash table if theres no established key pair
         hash[nums[i].toString()] = nums[i];
     }
     console.log(pairNumber);
