71f0d76fc6b9b1df5a24136cb2f839cc0a6b7cd6
kevinl622@gmail.com <kevinl622@gmail.com>
2018-10-09 21:55:46 -0700

    Updated comments

--- PlusOne.js
+++ PlusOne.js
@@ -36,7 +36,7 @@ var plusOne = function(digits) {
 
        // the end
       return digits;
-    }else{ // if the currentDigit is 9
+    } else { // if the currentDigit is 9
 
     // make it 0, and move to the former element
      digits[i] = 0;
@@ -47,7 +47,7 @@ var plusOne = function(digits) {
    if(addOneNow){
 
    // make [9,9,9], [1,0,0,0] not [0,0,0]
-     digits.unshift(1); 
+     digits.unshift(1);
    }
 
    return digits;
