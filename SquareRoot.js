47acd24249e5970fa7e6dde4461b47d17f9c3f1f
kevinl622@gmail.com <kevinl622@gmail.com>
2018-10-28 18:42:05 -0700

    Added Solution with Comment

---
+++ SquareRoot.js
@@ -0,0 +1,31 @@
+/*Square Root
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a
+non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only
+the integer part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since
+             the decimal part is truncated, 2 is returned.
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    //round down the squareroot in case it isn't a perfect square
+    let output = Math.floor(Math.sqrt(x));
+    console.log(output);
+    return output;
+};
